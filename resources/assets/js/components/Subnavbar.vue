6f3124c4d5185c042cb8d2dc6e0c1f2b23048c11
nachodd <nachodurand@gmail.com>
2018-06-20 11:47:16 -0300

    add bootstrap vue

---
+++ resources/assets/js/components/Subnavbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" fluid >
+      <p>For more information visit website</p>
+      <b-btn variant="primary" href="#">More Info</b-btn>
+    </b-jumbotron>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
